fc78cffe654e731bf5e5ac08b10161cc20f91401
Александрович Никита <nikikrad228@gmail.com>
2022-09-20 21:35:31 +0300

    created logIn and logOut but need make all checks enter login and password

--- app/src/main/java/com/example/animeproject/presentation/setting/login/LoginFragment.kt
+++ app/src/main/java/com/example/animeproject/presentation/setting/login/LoginFragment.kt
@@ -1,11 +1,13 @@
 package com.example.animeproject.presentation.setting.login
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.Navigation
 import com.example.animeproject.R
 import com.example.animeproject.databinding.FragmentLoginBinding
@@ -26,27 +28,66 @@ class LoginFragment : MvpAppCompatFragment(){
     lateinit var binding: FragmentLoginBinding
     lateinit var auth: FirebaseAuth
 
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+        auth = FirebaseAuth.getInstance()
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        checkLoggedInState()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        auth = FirebaseAuth.getInstance()
+
         binding.btnRegistration.setOnClickListener {
             Navigation.findNavController(binding.root).navigate(R.id.action_loginFragment_to_registrationFragment)
+        }
+        binding.btnLogOut.setOnClickListener {
+            auth.signOut()
+            checkLoggedInState()
+        }
+        binding.btnLogIn.setOnClickListener {
+            logIn()
         }
 
     }
 
-
-
-    override fun onStart() {
-        super.onStart()
-
+    private fun logIn(){
+        if(binding.etLogin.text !== null && binding.etPassword.text.length >= 6){
+            auth.signInWithEmailAndPassword(binding.etLogin.text.toString(), binding.etPassword.text.toString())
+            checkLoggedInState()
+        }else{
+            Toast.makeText(context , "Неправильный ввод данных!", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun checkLoggedInState() {
+        if (auth.currentUser !== null) {
+            binding.tvSignInLabel.text = "Вы онлайн!"
+            binding.etLogin.isVisible = false
+            binding.etPassword.isVisible = false
+            binding.btnLogIn.isVisible = false
+            binding.btnRegistration.isVisible = false
+            binding.btnLogOut.isVisible = true
+        } else {
+            binding.tvSignInLabel.text = "Войдите в аккаунт"
+            binding.etLogin.isVisible = true
+            binding.etPassword.isVisible = true
+            binding.btnLogIn.isVisible = true
+            binding.btnRegistration.isVisible = true
+            binding.btnLogOut.isVisible = false
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/animeproject/presentation/setting/registrate/RegistrationFragment.kt
+++ app/src/main/java/com/example/animeproject/presentation/setting/registrate/RegistrationFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.Navigation
+import com.example.animeproject.R
 import com.example.animeproject.databinding.FragmentRegistrationBinding
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.FirebaseApp
@@ -18,12 +20,10 @@ import moxy.MvpAppCompatFragment
 import java.io.FileInputStream
 
 
-class RegistrationFragment: MvpAppCompatFragment() {
+class RegistrationFragment : MvpAppCompatFragment() {
 
-    lateinit var binding: FragmentRegistrationBinding
-    lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInAccount: GoogleSignInAccount
-
+    private lateinit var binding: FragmentRegistrationBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,36 +40,41 @@ class RegistrationFragment: MvpAppCompatFragment() {
         binding.btnLogIn.setOnClickListener {
 
             if (binding.etRepeatPassword.text.toString() == binding.etPassword.text.toString()) {
-                registerUser()
-
+                if (binding.etPassword.text.toString().length < 6) {
+                    Toast.makeText(
+                        context,
+                        "Пароль должен быть больше 6 символов",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    registerUser()
+                }
             } else {
                 Toast.makeText(context, "Пароли не совпадают!", Toast.LENGTH_SHORT).show()
             }
         }
     }
-    private fun registerUser(){
+
+    private fun registerUser() {
         val email = binding.etLogin.text.toString()
         val password = binding.etPassword.text.toString()
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 auth.createUserWithEmailAndPassword(email, password)
-                withContext(Dispatchers.Main){
+                withContext(Dispatchers.Main) {
                     checkLoggedInState()
                 }
-            }catch (e: Exception){
-                withContext(Dispatchers.Main){
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
                     Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
 
-    fun checkLoggedInState(){
-        if(auth.currentUser == null){
-            binding.tvRegistrationLabel.text = "Хуй"
-        }else{
-            binding.tvRegistrationLabel.text = "Зарегался"
-
-        }
+    private fun checkLoggedInState() {
+        Toast.makeText(context, "Вы Зарегестрировались!", Toast.LENGTH_SHORT).show()
+        Navigation.findNavController(binding.root).popBackStack()
     }
+
 }
